62d81c4541dfe56a1969ff3638e29046e220bca9
jeevan <Jeevan-Kiran-Lenka>
2022-10-13 14:42:41 +0530

    Solution 2.4, 2.5
    Signed-off-by: jeevan <Jeevan-Kiran-Lenka>

--- part2/part2_a/src/2.4App.js
+++ part2/part2_a/src/2.4App.js
@@ -1,4 +1,38 @@
-import Courses from "./components/Courses"
+const Total = ({ parts }) => {
+  const total = parts.reduce((a, b) => a + b.exercises, 0)
+
+  return (
+    <div>
+      <p>
+        <b>total of {total} exercises</b>
+      </p>
+    </div>
+  )
+}
+
+const Course = ({ name, parts }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      {parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+const Courses = ({ courses }) => {
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course key={course.id} name={course.name} parts={course.parts} />
+      ))}
+    </div>
+  )
+}
 
 const App = () => {
   const courses = [
